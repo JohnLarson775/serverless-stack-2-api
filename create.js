f50075439d2da12e2aeb1d8118db7b7a0a0b3b93
MrJohnnyLarson <John.Larson.775@gmail.com>
2019-06-26 20:25:40 -0700

    Fixing the mistake

--- create.js
+++ create.js
@@ -2,8 +2,6 @@ import uuid from "uuid";
 import * as dynamoDbLib from "./libs/dynamodb-lib";
 import { success, failure } from "./libs/response-lib";
 
-gibberish.what;
-
 export async function main(event, context) {
   const data = JSON.parse(event.body);
   const params = {
